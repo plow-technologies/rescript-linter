3cf89109187f42b0058fbe643d6aafe50d4aba1c
Shulhi Sapli <shulhi@gmail.com>
2022-07-27 10:20:36 +0800

    Accept filename as CLI argument

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,12 @@
 open Rescript_linter
 
-let filename = "./test/foo.res"
+let run () =
+  let filename = ref "" in
+  let process_filename path = filename := path in
+  let usage = "Usage: " ^ Sys.argv.(0) ^ " <file>" in
+  Arg.parse [] process_filename usage;
+  match String.trim !filename with
+  | "" -> Arg.usage [] usage
+  | path -> Linter.run path
 
-let () = Linter.run filename
+let () = run ()
