da2b261ec74bb983f6148def3be6a1942262b971
Shulhi Sapli <shulhi@gmail.com>
2022-08-03 10:07:48 +0800

    Generalize jsx rule

--- lib/rules/JSXRule.ml
+++ lib/rules/JSXRule.ml
@@ -1,20 +1,28 @@
 open Rescript_parser
 
-module Rule : Rule.HASRULE with type t = Parsetree.expression = struct
+module Options = struct
+  type options = {component_name: string}
+end
+
+module Make (OPT : Rule.OPTIONS with type options = Options.options) :
+  Rule.HASRULE with type t = Parsetree.expression = struct
   type t = Parsetree.expression
 
   let proxy = Rule.MExpression
 
+  let component_name = OPT.options.Options.component_name
+
   let meta =
     { Rule.ruleName= "NoReactComponent"
-    ; Rule.ruleIdentifier= "NoReactComponent"
-    ; Rule.ruleDescription= "Don't use input type" }
+    ; Rule.ruleIdentifier= "NoReactComponent" ^ "[" ^ component_name ^ "]"
+    ; Rule.ruleDescription= "Don't use " ^ component_name ^ " component." }
 
   let lint expr =
     match expr with
-    | { Parsetree.pexp_desc= Pexp_apply ({pexp_desc= Pexp_ident {txt= Longident.Lident "input"}}, _)
+    | { Parsetree.pexp_desc= Pexp_apply ({pexp_desc= Pexp_ident {txt= Longident.Lident ident}}, _)
       ; Parsetree.pexp_attributes= [({Asttypes.txt= "JSX"}, _)]
-      ; Parsetree.pexp_loc= loc } ->
+      ; Parsetree.pexp_loc= loc }
+      when ident = component_name ->
         Rule.LintError (meta.ruleDescription, loc)
     | _ -> Rule.LintOk
 end
