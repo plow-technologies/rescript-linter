bdb551e6d6147c4613cfe9ad1d8d899c13dce833
Shulhi Sapli <shulhi@gmail.com>
2022-08-05 12:24:25 +0800

    Handle third case of module linting

--- lib/rules/DisallowModuleRule.ml
+++ lib/rules/DisallowModuleRule.ml
@@ -19,31 +19,59 @@ module Make (OPT : Rule.OPTIONS with type options = Options.options) : Rule.HASR
 
   (* There are three cases that we need to handle when linting for module usage (assume M is the module name)
 
-     1. M.function 2. open M 3. module N = M *)
+     1. open M
+     2. module N = M
+     3. M.function or M.attribute
+
+     For 3, we might have false positive because the lint looks for `Pexp_ident` with the module name.
+
+     For example, if you something like
+
+     ```rescript
+     // shadow existing module CssJs
+     module CssJs = {
+       let func () = ...
+     }
+
+     let _ = CssJs.func()
+     ```
+
+     This will trigger lint error on line `let _ = CssJs.func()`. Anyway I don't think this is a good pattern anyway to shadow existing
+     module.
+  *)
   let lintStructureItem =
     Rule.LintStructureItem
       (fun expr ->
         match expr with
         (* match open M *)
-        | {Parsetree.pstr_desc= Parsetree.Pstr_open {Parsetree.popen_lid= {txt= Longident.Lident _ident}}} ->
-            Rule.LintOk
+        | { Parsetree.pstr_desc=
+              Parsetree.Pstr_open
+                {Parsetree.popen_lid= {txt= Longident.Lident ident}; Parsetree.popen_loc= loc} }
+          when ident = op ->
+            Rule.LintError (meta.ruleDescription, loc)
         (* match J = M *)
         | { Parsetree.pstr_desc=
               Parsetree.Pstr_module
                 { Parsetree.pmb_expr=
-                    {Parsetree.pmod_desc= Parsetree.Pmod_ident {txt= Longident.Lident _ident}} } } ->
-            Rule.LintOk
+                    { Parsetree.pmod_desc= Parsetree.Pmod_ident {txt= Longident.Lident ident}
+                    ; Parsetree.pmod_loc= loc } } }
+          when ident = op ->
+            Rule.LintError (meta.ruleDescription, loc)
         | _ -> Rule.LintOk )
 
-  let lint = [lintStructureItem]
-end
+  let lintExpression =
+    Rule.LintExpression
+      (fun expr ->
+        match expr with
+        (* match M.function or M.attribute *)
+        | { Parsetree.pexp_desc=
+              Pexp_apply
+                ( { pexp_desc= Pexp_ident {txt= Longident.Ldot (Longident.Lident ident, _)}
+                  ; Parsetree.pexp_loc= loc }
+                , _ ) }
+          when ident = op ->
+            Rule.LintError (meta.ruleDescription, loc)
+        | _ -> Rule.LintOk )
 
-(* structure_item (test.res[21,300+2]..[21,300+51]) Pstr_value Nonrec [ <def> pattern
-   (test.res[21,300+6]..[21,300+7]) Ppat_any expression (test.res[21,300+10]..[21,300+51]) attribute "bs"
-   (_none_[1,0+-1]..[1,0+-1]) ghost [] Pexp_apply expression (test.res[21,300+10]..[21,300+21]) Pexp_ident
-   "CssJs.style" (test.res[21,300+10]..[21,300+21]) [ <arg> Nolabel expression
-   (test.res[21,300+24]..[21,300+50]) Pexp_array [ expression (test.res[21,300+25]..[21,300+49]) Pexp_apply
-   expression (test.res[21,300+25]..[21,300+41]) Pexp_ident "CssJs.marginLeft"
-   (test.res[21,300+25]..[21,300+41]) [ <arg> Nolabel expression (test.res[21,300+42]..[21,300+48])
-   Pexp_variant "px" Some expression (test.res[21,300+46]..[21,300+47]) Pexp_constant PConst_int (0,None) ] ]
-   ] ] ] *)
+  let lint = [lintStructureItem; lintExpression]
+end
